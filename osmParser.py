d71a0e71e0ca3e848e09cba2619451a00edd3df4
UsernameTakenOof <weibert.alexander@web.de>
2023-04-22 16:17:24 +0200

    initial push
    added osm test file, osm fuwa file and osmParser.py

---
+++ osmParser.py
@@ -0,0 +1,13 @@
+import xml.etree.ElementTree as ET
+
+# Parse the XML file
+tree = ET.parse('test.osm')
+
+# Get the root element
+root = tree.getroot()
+
+# Iterate over the child elements of the root
+for child in root:
+    print(child.tag, child.attrib, "\n")
+    for sub_child in child:
+        print(sub_child.tag, sub_child.text, "\n")
