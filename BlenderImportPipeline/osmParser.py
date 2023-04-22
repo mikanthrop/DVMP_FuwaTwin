8cb03ead47885a794c9b8c7d57846ba46adc4ec7
UsernameTakenOof <weibert.alexander@web.de>
2023-04-22 17:39:30 +0200

    Update osmParser.py
    added parser for osm nodes and ways, also places cubes in blender

--- BlenderImportPipeline/osmParser.py
+++ BlenderImportPipeline/osmParser.py
@@ -1,14 +1,39 @@
 import xml.etree.ElementTree as ET
 import bpy
+import numpy as np
 
 # Parse the XML file
-tree = ET.parse('test.osm')
+tree = ET.parse("BlenderImportPipeline/testMaps/map.osm")
 
-# Get the root element
+node_dict = {}
+
+# Get the root element>
 root = tree.getroot()
 
+firstNodeLocation = ()
+scalingFactor = 10000
 # Iterate over the child elements of the root
 for child in root:
-    print(child.tag, child.attrib, "\n")
-    for sub_child in child:
-        print(sub_child.tag, sub_child.text)
+    if (child.tag == "node"):
+        # Store first node location, used for centering map
+        if (firstNodeLocation == ()):
+            lat = child.attrib.get("lat")
+            lon = child.attrib.get("lon")
+            firstNodeLocation = (np.float64(lat), np.float64(lon))
+        # Store key and value pairs
+        keyString = child.attrib.get("id")
+        valueString = child.attrib.get("lat") + " " + child.attrib.get("lon")
+        node_dict[keyString] = valueString
+    elif (child.tag == "way"):
+        for c in child:
+            nodeRef = c.attrib.get("ref")
+            if (nodeRef != None):
+                node = node_dict[nodeRef]
+                print(node)
+                lat, lon = node.split()
+                fittedLat = (np.float64(lat) - firstNodeLocation[0]) * scalingFactor
+                fittedLon = (np.float64(lon) - firstNodeLocation[1]) * scalingFactor
+
+                #print(fittedLat, " ", fittedLon)
+                bpy.ops.mesh.primitive_cube_add(location=(np.float64(lat), np.float64(lon), 0), scale=(1, 1, 1))
+
