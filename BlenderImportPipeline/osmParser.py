ddd295f23cf5a1e8dfd5e1c6ae1c4e77eb0c7aa1
UsernameTakenOof <weibert.alexander@web.de>
2023-06-17 18:05:46 +0200

    added terrain

--- BlenderImportPipeline/osmParser.py
+++ BlenderImportPipeline/osmParser.py
@@ -1,7 +1,7 @@
 import xml.etree.ElementTree as ET
 import bpy
 import bmesh
-import numpy as np
+import math
 
 class OSMParser():
 
@@ -19,14 +19,14 @@ class OSMParser():
         self.forbidden = ["forest", "meadow", "park", "grassland"]
         self.allowed = ["building", "highway"]
         
-        self.tree = ET.parse("../BlenderImportPipeline/testMaps/map.osm")
+        self.tree = ET.parse("BlenderImportPipeline/testMaps/map.osm")
 
 
     def loadTerrain(self):
         """
         Loads height data from terrainData.asc.
         """
-        file = open("../BlenderImportPipeline/terrain/terrainData.asc")
+        file = open("BlenderImportPipeline/terrain/terrainData.asc")
         lines = file.readlines()
 
         self.ncols = int(lines[0].split()[-1])
@@ -72,13 +72,50 @@ class OSMParser():
         """
         latDiff = lat - self.minLat
         lonDiff = lon - self.minLon
-        latIdx = latDiff // self.cellSize
-        lonIdx = lonDiff // self.cellSize
-        if (latIdx >= self.nrows):
-            latIdx = self.nrows - 1
-        if(lonIdx >= self.ncols):
-            lonIdx = self.ncols - 1
-        return self.terrainMap[int(latIdx)][int(lonIdx)]
+
+        latIdx = latDiff / self.cellSize
+        lonIdx = lonDiff / self.cellSize
+
+        latFloor = int(math.floor(latIdx))
+        latCeil = int(math.ceil(latIdx))
+        lonFloor = int(math.floor(lonIdx))
+        lonCeil = int(math.ceil(lonIdx))
+
+        if(lonCeil >= self.ncols):
+            lonFloor = self.ncols - 2
+            lonCeil = self.ncols - 1
+        if(latCeil >= self.nrows):
+            latFloor = self.nrows - 2
+            latCeil = self.nrows - 1
+
+        #print(latFloor, latCeil, lonFloor, lonCeil)
+        heightv1 = int(self.terrainMap[latFloor][lonFloor])
+        heightv2 = int(self.terrainMap[latCeil][lonFloor])
+        heightv3 = int(self.terrainMap[latFloor][lonCeil])
+        heightv4 = int(self.terrainMap[latCeil][lonCeil])
+
+        # xFloor =  self.minLat * latFloor * self.cellSize
+        # xCeil = self.minLat * latCeil * self.cellSize
+        # yFloor = self.minLon * lonFloor * self.cellSize
+        # yCeil = self.minLon * lonCeil * self.cellSize
+
+        # v1Coords = (xFloor, yFloor)
+        # v2Coords = (xCeil, yFloor)
+        # v3Coords = (xFloor, yCeil)
+        # v4Coords = (xCeil, yCeil)
+
+        dist1 = latIdx - latFloor
+        dist2 = latCeil - latIdx
+        dist3 = lonIdx - lonFloor
+        dist4 = lonCeil - lonIdx
+
+        area1 = dist2 * dist4
+        area2 = dist1 * dist4
+        area3 = dist2 * dist3
+        area4 = dist1 * dist3
+
+        return area1 * heightv1 + area2 * heightv2 + area3 * heightv3 + area4 * heightv4
+
 
     def parse(self):
         """
@@ -93,7 +130,7 @@ class OSMParser():
         streetIndex = -1
 
         firstNodeLocation = ()
-        scalingFactor = 10000
+        scalingFactor = 100000
 
         # Iterate over the child elements of the root
         for child in root:
@@ -103,7 +140,7 @@ class OSMParser():
                 if (firstNodeLocation == ()):
                     lat = child.attrib.get("lat")
                     lon = child.attrib.get("lon")
-                    firstNodeLocation = (np.float64(lat), np.float64(lon))
+                    firstNodeLocation = (float(lat), float(lon))
 
                 # Store key and value pairs
                 keyString = child.attrib.get("id")
@@ -123,8 +160,11 @@ class OSMParser():
                 if allowedFound:
                     nodeRefs = child.findall("nd")
                     usedNodes = []
-
                     newObject = True
+                    
+
+                    firstIndexFound = False
+                    firstIndex = 0
 
                     for nd in nodeRefs:
                         nodeRef = nd.get("ref")
@@ -139,13 +179,15 @@ class OSMParser():
                                         firstNodeLocation[1]) * scalingFactor
                             
                             mappedHgt = self.calculate_height(lat, lon)
-                            mappedHgt = -float(mappedHgt) / 10
+                            mappedHgt = -float(mappedHgt)
                             
                             if tagString == "building":
                                 if nodeRef not in usedNodes:
                                     usedNodes.append(nodeRef)
                                     self.buildingObject.verts.new((fittedLat, fittedLon, mappedHgt))
                                     buildingIndex += 1
+                                    if not firstIndexFound:
+                                        firstIndex = buildingIndex
                                 if not newObject:
                                     self.buildingObject.verts.ensure_lookup_table()
                                     if not self.building_edge_exists(self.buildingObject.verts[buildingIndex - 1], self.buildingObject.verts[buildingIndex]):
@@ -162,7 +204,11 @@ class OSMParser():
                                         self.streetObject.edges.new((self.streetObject.verts[streetIndex - 1], self.streetObject.verts[streetIndex]))
                             newObject = False
                         else:
-                            continue
+                            if tagString == "building":
+                                self.buildingObject.verts.ensure_lookup_table()
+                                if not self.building_edge_exists(self.buildingObject.verts[firstIndex], self.buildingObject.verts[buildingIndex]):
+                                    self.buildingObject.edges.new((self.buildingObject.verts[firstIndex], self.buildingObject.verts[buildingIndex - 1]))
+
                 else:
                     continue
 
