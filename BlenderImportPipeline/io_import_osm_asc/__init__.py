9a4191341268a9312d48bda7d2341fc2094d700a
mikanthrop <marie.vera.monika.cleppien@hs-furtwangen.de>
2023-06-26 18:40:15 +0200

    added working sidebar
    sidebar contains
    - slider for storey height
    - slider for scaling factor
    - button to import FuWa

--- BlenderImportPipeline/io_import_osm_asc/__init__.py
+++ BlenderImportPipeline/io_import_osm_asc/__init__.py
@@ -18,15 +18,17 @@ def menu_func_osm_asc_import(self, context):
 
 def register():
     bpy.utils.register_class(osm_asc_importer.OSM_ASC_OT_ImportOperator)
-  #  bpy.utils.register_class(osm_asc_importer.OSM_ASC_PT_Panel)
-    bpy.utils.register_manual_map(osm_asc_importer.add_object_manual_map)
+    bpy.utils.register_class(osm_asc_importer.OSM_ASC_PT_Panel)
     bpy.types.TOPBAR_MT_file_import.append(menu_func_osm_asc_import)
-    
+    bpy.utils.register_class(osm_asc_importer.OSM_ASC_Properties)
+    bpy.types.Scene.osm_asc_props = bpy.props.PointerProperty(type=osm_asc_importer.OSM_ASC_Properties)
+
 def unregister():
     bpy.utils.unregister_class(osm_asc_importer.OSM_ASC_OT_ImportOperator)
-  #  bpy.utils.unregister_class(osm_asc_importer.OSM_ASC_PT_Panel)
-    bpy.utils.unregister_manual_map(osm_asc_importer.add_object_manual_map)
+    bpy.utils.unregister_class(osm_asc_importer.OSM_ASC_PT_Panel)
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_osm_asc_import)
+    bpy.utils.unregister_class(osm_asc_importer.OSM_ASC_Properties)
+    del bpy.types.Scene.osm_asc_props
 
 if __name__ == "__main__":
     register()
--- BlenderImportPipeline/io_import_osm_asc/osm_asc_importer.py
+++ BlenderImportPipeline/io_import_osm_asc/osm_asc_importer.py
@@ -1,19 +1,21 @@
-import bpy
+bl_info = {
+    "name": "OSM and ASC Map with Terrain Import Mockup",
+    "author": "Alexander Weibert, Hau-David Nguyen, Marie Cleppien",
+    "version": (1, 0),
+    "blender": (2, 93, 0),
+    "location": "File > Import > OSM (.osm)",
+    "description": "Imports Open Street View Map into Blender",
+    "warning": "",
+    "doc_url": "",
+    "category": "Import",
+}
+
+import bpy 
 from bpy.types import Operator, Panel, PropertyGroup
-from bpy.props import FloatProperty, BoolProperty #, StringProperty
+from bpy.props import FloatProperty, BoolProperty
 from . import osmParser
 
-# Operator
-class OSM_ASC_OT_ImportOperator(Operator):
-    """Create a scene from an OSM and ASC file"""
-    bl_idname = "import_scene.osm_asc"
-    bl_label = "Import OSM and ASC"
-    bl_description = "Imports an OSM and ASC file"
-    bl_options = {'REGISTER', 'UNDO'}
-
-    def __init__(self) -> None:
-        super().__init__()
-
+class OSM_ASC_Properties(PropertyGroup):
     scalingFactor: FloatProperty(
         name="Scaling Factor",
         description="Scaling of the imported scene",
@@ -27,7 +29,7 @@ class OSM_ASC_OT_ImportOperator(Operator):
         description="Height of one storey",
         default=2.4,
         min=1,
-        max=10,
+        max=100,
     )
 
     highlightHFU: BoolProperty(
@@ -36,38 +38,40 @@ class OSM_ASC_OT_ImportOperator(Operator):
         default=True
     )
 
+class OSM_ASC_OT_ImportOperator(Operator):
+    """Create a scene from an OSM and ASC file"""
+    bl_idname = "import_scene.osm_asc"
+    bl_label = "Import OSM and ASC"
+    bl_description = "Imports an OSM and ASC file"
+    bl_options = {'REGISTER', 'UNDO'}
+    
     def execute(self, context):
-        # props = context.window_manager.osm_asc_props
-        
-        storey_height = self.storeyHeight
-        scaling_factor = self.scalingFactor
-        highlight_hfu = self.highlightHFU
+        props = context.scene.osm_asc_props
+
+        storey_height = props.storeyHeight
+        scaling_factor = props.scalingFactor
+        highlight_hfu = props.highlightHFU
         parser = osmParser.OSMParser(storey_height, scaling_factor, highlight_hfu)
         parser.parse()
         return {'FINISHED'}
+    
 
-# class OSM_ASC_PT_Panel(Panel):
-#     bl_idname = "OSM_ASC_PT_Panel"
-#     bl_label = "OSM and ASC Importer"
-#     bl_space_type = 'VIEW_3D'
-#     bl_region_type = 'UI'
-#     bl_category = 'Tool'
+class OSM_ASC_PT_Panel(Panel):
+    bl_idname = "OSM_ASC_PT_Panel"
+    bl_label = "OSM and ASC Importer"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'OSM & ASC Importer'
 
-#     def draw(self, context):
-#         layout = self.layout
-#         scene = context.scene
-#         operator = scene.operator("import_scene.osm_asc")
-#         box = layout.box()
-
-#         box.prop(operator, "storeyHeight")
-#         box.prop(operator, "scalingFactor")
-#         box.prop(operator, "highlightHFU")
-#         box.operator("import_scene.osm_asc", text="Import")
-
-# This allows you to right click on a button and link to documentation
-def add_object_manual_map():
-    url_manual_prefix = "https://docs.blender.org/manual/en/latest/"
-    url_manual_mapping = (
-        ("bpy.ops.mesh.add_object", "scene_layout/object/types.html"),
-    )
-    return url_manual_prefix, url_manual_mapping
+    def draw(self, context):
+        layout = self.layout
+        obj = context.object
+        props = context.scene.osm_asc_props
+        
+        layout.row().label(text="FuWaTwin Config")
+        layout.prop(props, "storeyHeight")
+        layout.prop(props, "scalingFactor")
+        layout.prop(props, "highlightHFU")
+        
+        self.layout.separator()
+        layout.row().operator("import_scene.osm_asc", text="Import")
