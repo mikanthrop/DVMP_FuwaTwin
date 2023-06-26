b8942a15decf993c7d4aa034aab7f5a938b2013b
mikanthrop <marie.vera.monika.cleppien@hs-furtwangen.de>
2023-06-26 13:12:39 +0200

    updated osm_asc_importer to show ui
    unsuccessful, doesn't draw ui at all

--- BlenderImportPipeline/io_import_osm_asc/__init__.py
+++ BlenderImportPipeline/io_import_osm_asc/__init__.py
@@ -11,24 +11,22 @@ bl_info = {
 }
 
 import bpy
-from .osm_asc_importer import OSM_ASC_OT_ImportOperator
-# from .osm_asc_importer import IMPORT_PT_ImportPanel
+from . import osm_asc_importer
 
 def menu_func_osm_asc_import(self, context):
-    self.layout.operator(OSM_ASC_OT_ImportOperator.bl_idname, text="OpenStreetMap (.osm) & Arc ASCII Grid (.asc)")
-
-# def menu_func_asc_import(self, context):
-#     self.layout.operator(ASC_OT_ImportOperator.bl_idname, text="Arc ASCII Grid (.asc)")
+    self.layout.operator(osm_asc_importer.OSM_ASC_OT_ImportOperator.bl_idname, text="OpenStreetMap (.osm) & Arc ASCII Grid (.asc)")
 
 def register():
-    bpy.utils.register_class(OSM_ASC_OT_ImportOperator)
+    bpy.utils.register_class(osm_asc_importer.OSM_ASC_OT_ImportOperator)
+  #  bpy.utils.register_class(osm_asc_importer.OSM_ASC_PT_Panel)
+    bpy.utils.register_manual_map(osm_asc_importer.add_object_manual_map)
     bpy.types.TOPBAR_MT_file_import.append(menu_func_osm_asc_import)
-    # bpy.utils.register_class(IMPORT_PT_ImportPanel)
     
 def unregister():
-    bpy.utils.unregister_class(OSM_ASC_OT_ImportOperator)
+    bpy.utils.unregister_class(osm_asc_importer.OSM_ASC_OT_ImportOperator)
+  #  bpy.utils.unregister_class(osm_asc_importer.OSM_ASC_PT_Panel)
+    bpy.utils.unregister_manual_map(osm_asc_importer.add_object_manual_map)
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_osm_asc_import)
-    # bpy.utils.unregister_class(IMPORT_PT_ImportPanel)
 
 if __name__ == "__main__":
     register()
--- BlenderImportPipeline/io_import_osm_asc/osm_asc_importer.py
+++ BlenderImportPipeline/io_import_osm_asc/osm_asc_importer.py
@@ -1,32 +1,18 @@
 import bpy
-from bpy.types import Operator, Panel
-from bpy.props import StringProperty, FloatProperty, BoolProperty
+from bpy.types import Operator, Panel, PropertyGroup
+from bpy.props import FloatProperty, BoolProperty #, StringProperty
 from . import osmParser
-
-def import_osm_file(self, filepath):
-    print("Importing OSM file:", filepath)
-    bpy.ops.import_scene.osm(filepath=filepath)
-
-def import_asc_file(self, filepath):
-    print("Importing ASC file:", filepath)
-    bpy.ops.import_scene.asc(filepath=filepath)
 
 # Operator
 class OSM_ASC_OT_ImportOperator(Operator):
     """Create a scene from an OSM and ASC file"""
     bl_idname = "import_scene.osm_asc"
     bl_label = "Import OSM and ASC"
-    bl_description = "Import an OSM and ASC file"
+    bl_description = "Imports an OSM and ASC file"
     bl_options = {'REGISTER', 'UNDO'}
 
-    # filepath_osm: StringProperty(
-    #     subtype="FILE_PATH", 
-    #     name="OSM File"
-    #     )
-    # filepath_asc: StringProperty(
-    #     subtype="FILE_PATH", 
-    #     name="ASC File"
-    #     )
+    def __init__(self) -> None:
+        super().__init__()
 
     scalingFactor: FloatProperty(
         name="Scaling Factor",
@@ -34,7 +20,6 @@ class OSM_ASC_OT_ImportOperator(Operator):
         default=10000,
         min=1,
         max=1000000,
-        subtype='UNSIGNED',
     )
 
     storeyHeight: FloatProperty(
@@ -43,7 +28,6 @@ class OSM_ASC_OT_ImportOperator(Operator):
         default=2.4,
         min=1,
         max=10,
-        subtype='UNSIGNED'
     )
 
     highlightHFU: BoolProperty(
@@ -53,16 +37,32 @@ class OSM_ASC_OT_ImportOperator(Operator):
     )
 
     def execute(self, context):
-        # osm_file = self.filepath_osm
-        # asc_file = self.filepath_asc
-        # Call the osmParser function with the OSM and ASC file paths
-        # osmParser(osm_file, asc_file)
-        print("parsing osm file")
-        parser = osmParser.OSMParser(self.storeyHeight, self.scalingFactor, self.highlightHFU)
+        # props = context.window_manager.osm_asc_props
+        
+        storey_height = self.storeyHeight
+        scaling_factor = self.scalingFactor
+        highlight_hfu = self.highlightHFU
+        parser = osmParser.OSMParser(storey_height, scaling_factor, highlight_hfu)
         parser.parse()
-
-
         return {'FINISHED'}
+
+# class OSM_ASC_PT_Panel(Panel):
+#     bl_idname = "OSM_ASC_PT_Panel"
+#     bl_label = "OSM and ASC Importer"
+#     bl_space_type = 'VIEW_3D'
+#     bl_region_type = 'UI'
+#     bl_category = 'Tool'
+
+#     def draw(self, context):
+#         layout = self.layout
+#         scene = context.scene
+#         operator = scene.operator("import_scene.osm_asc")
+#         box = layout.box()
+
+#         box.prop(operator, "storeyHeight")
+#         box.prop(operator, "scalingFactor")
+#         box.prop(operator, "highlightHFU")
+#         box.operator("import_scene.osm_asc", text="Import")
 
 # This allows you to right click on a button and link to documentation
 def add_object_manual_map():
@@ -71,19 +71,3 @@ def add_object_manual_map():
         ("bpy.ops.mesh.add_object", "scene_layout/object/types.html"),
     )
     return url_manual_prefix, url_manual_mapping
-
-def register():
-    bpy.utils.register_class(OSM_ASC_OT_ImportOperator)
-    bpy.utils.register_manual_map(add_object_manual_map)
-    #bpy.types.VIEW3D_MT_mesh_add.append(add_object_button)
-
-def unregister():
-    bpy.utils.unregister_class(OSM_ASC_OT_ImportOperator)
-    bpy.utils.unregister_manual_map(add_object_manual_map)
-    #bpy.types.VIEW3D_MT_mesh_add.remove(add_object_button)
-
-def menu_func_import(self, context):
-    self.layout.operator(OSM_ASC_OT_ImportOperator.bl_idname, text="OpenStreetMap and ASC (.osm, .asc)")
-
-if __name__ == "__main__":
-    OSM_ASC_OT_ImportOperator()
